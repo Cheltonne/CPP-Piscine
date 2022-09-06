88b42317bf96f29a335effd5829863c233d40cc2
Cheltonne <chelton1506@gmail.com>
2022-09-06 13:42:11 +0200

    first commit2

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: chajax <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 11:18:02 by chajax            #+#    #+#             */
-/*   Updated: 2022/09/06 11:53:46 by chajax           ###   ########.fr       */
+/*   Updated: 2022/09/06 13:41:51 by chajax           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,38 +34,42 @@ class Array
 			this->_array = NULL;
 			this->_size = 0;
 		};
-		unsigned int getSize(void) const
+		unsigned int size(void) const
 		{
 			return (this->_size);
 		}
-		T &operator[](const unsigned int index)
+
+		T	&operator[](const size_t index)
 		{
 			if (index < 0 || index >= this->_size)
 				throw (std::exception());
 			return (this->_array[index]);
 		}
-		Array<T> (T &cpy)
+		Array<T>	&operator=(const Array<T> &rhs )
+		{
+			std::cout << "Assignment = operator called\n";
+			if (this == &rhs)
+				return (*this);
+			this->_size = rhs.size();
+			this->_array = new T[this->_size];
+			for (size_t i = 0; i < this->size(); i++)
+				this->_array[i] = rhs._array[i];
+			return (*this);
+		}
+
+		Array<T> (Array<T> &cpy)
 		{
 			std::cout << "Copy constructor called\n";
 			*this = cpy;
 			return (*this);
 		}
-		T &operator=(T const &rhs )
-		{
-			std::cout << "Assignement = operator called\n";
-			if (*this == rhs)
-				return (*this);
-			if (this->_size > 0)
-				delete [] this->_array;
-			this->_size = rhs.getSize();
-			this->_array = new T[this->_size];
-			for (int i = 0; i < this->_array; i++)
-				this->_array[i] = rhs[i];
-			return (*this);
-		}
+
 		~Array()
 		{
 			std::cout << "Array destructor called" << '\n';
+			std::cout << "Array size = " << size() << '\n';
+			if (this->size() > 0)
+				delete [] (this->_array);
 		};
 };
 
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chajax <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 11:42:03 by chajax            #+#    #+#             */
-/*   Updated: 2022/09/06 11:54:34 by chajax           ###   ########.fr       */
+/*   Updated: 2022/09/06 12:52:59 by chajax           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ int	main(void)
 {
 	Array<int>	emptyArr;
 	std::cout << "empyArr " <<
-		(emptyArr.getSize() == 0 ? "is empty" : "is not empty") << '\n';
+		(emptyArr.size() == 0 ? "is empty" : "is not empty") << '\n';
 
 	std::cout << '\n';
 
 	Array<int>	intArr(4);
-	std::cout << "Size of intArr..." << intArr.getSize() << '\n';
+	std::cout << "Size of intArr..." << intArr.size() << '\n';
 
 	std::cout << "Value of intArr[0]: " << intArr[0] << '\n';
 	intArr[0] = 1;
 	intArr[1] = 2;
 	intArr[2] = 3;
 	intArr[3] = 4;
-	std::cout << "Size of intArr..." << intArr.getSize() << '\n';
+	std::cout << "Size of intArr..." << intArr.size() << '\n';
 
 	std::cout << '\n';
 
@@ -45,7 +45,7 @@ int	main(void)
 
 	std::cout << '\n';
 
-	for (size_t i(0); i < intArr.getSize(); ++i)
+	for (size_t i(0); i < intArr.size(); ++i)
 	{
 		try { std::cout << "intArr[" << i << "]: " << intArr[i] << '\n'; }
 		catch (std::exception &e) { std::cout << e.what() << '\n'; }
@@ -54,14 +54,14 @@ int	main(void)
 	std::cout << '\n';
 
 	Array<int>	cpyArr;
-	std::cout << "Size of cpyArr..." << cpyArr.getSize() << '\n';
+	std::cout << "Size of cpyArr..." << cpyArr.size() << '\n';
 	std::cout << "\033[36mDeep copying intArr to cpyArr...\033[0m" << '\n';
 	cpyArr = intArr;
-	std::cout << "New _size of cpyArr..." << cpyArr.getSize() << '\n';
+	std::cout << "New _size of cpyArr..." << cpyArr.size() << '\n';
 
 	std::cout << "\033[36mChanging value in cpyArr...\033[0m" << '\n';
 	cpyArr[1] = 42;
-	for (size_t i(0); i < intArr.getSize(); ++i)
+	for (size_t i(0); i < intArr.size(); ++i)
 	{
 		try { std::cout << "cpyArr[" << i << "]: " << cpyArr[i] << '\n'; }
 		catch (std::exception &e) { std::cout << e.what() << '\n'; }
@@ -70,14 +70,12 @@ int	main(void)
 	std::cout << '\n';
 
 	std::cout << "\033[36mChecking if intArray has been modified...\033[0m" << '\n';
-	for (size_t i(0); i < intArr.getSize(); ++i)
+	for (size_t i(0); i < intArr.size(); ++i)
 	{
 		try { std::cout << "intArr[" << i << "]: " << intArr[i] << '\n'; }
 		catch (std::exception &e) { std::cout << e.what() << '\n'; }
 	}
 
-
 	std::cout << '\n';
-
 	return 0;
 }
